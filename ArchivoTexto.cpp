db191e69be119568fa48c221062ea7dc0d9a1233
MarioVelasquezN <41805686+MarioVelasquezN@users.noreply.github.com>
2020-03-23 14:34:38 -0600

    Update ArchivoTexto.cpp

--- ArchivoTexto.cpp
+++ ArchivoTexto.cpp
@@ -12,7 +12,7 @@ void ReadAll() {
 	uint16_t r_size;
 	Empleado p;
 	ifstream f;
-	cout << "Nombres  Apellidos  Cedula Sueldo Edad" << endl;
+	cout << "Nombres  Apellidos  Direccion Ciudad Estado ZipCode" << endl;
 
 	f.open("empleado.txt");
 
@@ -89,10 +89,45 @@ void Empleado::GetEmpleadoByName(string nombre) {
 	f.close();
 }
 
+void Empleado::EliminarRegistro() {
+	ifstream emple;
+	emple.open("empleado.txt", ios::in);
+	char aux[10];
+
+	ofstream entrada;
+	entrada.open("temp.txt", ios::out);
+	char marca ='*';
+	if (emple.fail()) {
+		cout << "Error al abrir el archivo.";
+		return;
+	}
+	else {
+		cout << "Introduzca el nombre: ";
+		cin >> aux;
+
+		emple >> nombre;
+		while (emple.eof()) {
+			emple >> Apellido >> direccion >> ciudad >> estado >> zipCode;
+
+			if (strcmp(aux, nombre) == 0) {
+				entrada <<marca;
+			}
+			else {
+				entrada <<nombre<< Apellido << direccion << ciudad << estado << zipCode;
+			}
+			emple >> nombre;
+		}
+		emple.close();
+		entrada.close();
+
+	}
+}
+
 int main(int argc, char **argv) {
 	Empleado p;
 
 	if (strcmp(argv[1], "1") == 0) ReadAll();
 	if (strcmp(argv[1], "2") == 0) WriteAll();
 	if (strcmp(argv[1], "3") == 0) p.GetEmpleadoByName("Mario");
+	if (strcmp(argv[1], "4") == 0) p.EliminarRegistro();
 }
